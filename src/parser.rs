d3057193f07c0278679ef1c8d7e4a63a3939a655
Jerry Yang <efjerryyang@outlook.com>
2024-03-06 00:36:16 -0800

    feat: add grammar and parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,2 +1,16 @@
 mod ast;
 mod visitor;
+
+use pest::Parser;
+use pest_derive::Parser;
+use pest::error::Error;
+
+#[derive(Parser)]
+#[grammar = "parser/grammar.pest"] // relative to project `src`
+pub struct SyGuSParser;
+
+pub fn parse(input: &str) -> Result<ast::SyGuS, Error<Rule>> {
+    let mut pairs = SyGuSParser::parse(Rule::main, input)?;
+    let pair = pairs.next().unwrap();
+    visitor::visit_main(pair)
+}
